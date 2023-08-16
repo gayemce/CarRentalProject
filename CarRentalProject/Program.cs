4b4ed53825cc3fd79565bfcc135aea883fb88b48
gayemce <100995170+gayemce@users.noreply.github.com>
2023-08-16 09:12:13 +0300

    Added `ıslemler` method

--- CarRentalProject/Program.cs
+++ CarRentalProject/Program.cs
@@ -9,6 +9,17 @@
             public int motorGucu;
         }
 
+         private static void ıslemler()
+        {
+            Console.WriteLine("*** İşlemler *** ");
+            Console.WriteLine("1 - Araç Listesi");
+            Console.WriteLine("2 - Araç Sayısı");
+            Console.WriteLine("3 - Araç Ekle");
+            Console.WriteLine("4 - Araç Kirala");
+            Console.WriteLine("5 - İşlemleri Göster");
+            Console.WriteLine("6 - Çıkış\n\n");
+        }
+
         static void Main(string[] args)
         {
             

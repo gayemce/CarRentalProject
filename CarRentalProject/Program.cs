c50e0144428c316ee902718fdbf49858fc40b8f8
= <=>
2023-08-16 09:06:01 +0300

    Add project files.

---
+++ CarRentalProject/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRentalProject
+{
+    internal class Program
+    {
+        public class Car
+        {
+            public string marka;
+            public int model;
+            public int motorGucu;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
